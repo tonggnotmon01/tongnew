25d96ff4ff298793df0230f46365ec2e79d85b9e
LawAng <tong.not.dead@gmail.com>
2019-04-04 12:38:37 +0700

    map bsru new

--- app/src/main/java/masterung/th/in/androidthai/bsruservice/MapsActivity.java
+++ app/src/main/java/masterung/th/in/androidthai/bsruservice/MapsActivity.java
@@ -15,6 +15,7 @@ import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
@@ -26,6 +27,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     private double myLat = 0, myLng = 0;
     private LocationManager locationManager;
     private Criteria criteria;
+    private int[] plateInts = {R.drawable.imos, R.drawable.iaaa, R.drawable.itong};
 
 
     @Override
@@ -145,5 +147,21 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         LatLng bsruLatLng = new LatLng(latBSRU = 13.733141,lngBSRU = 100.489417);
         mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(bsruLatLng, 16));
 
+        if (myLat !=0)  {
+            createMarker(myLat, myLng, plateInts[0]);
+        }
+
     }   // onMapReady
+
+    private void createMarker(double lat, double lng, int iconPlace) {
+
+        LatLng latLng = new LatLng(lat, lng);
+        MarkerOptions markerOptions = new MarkerOptions()
+                .position(latLng)
+                .icon(BitmapDescriptorFactory.fromResource(iconPlace));
+        mMap.addMarker(markerOptions);
+
+
+
+    }
 }
